256a3d76b8729a51e3b388916cdbed2fd11bace8
ahmadliaydin <aydin.ahmadli@merck.com>
2021-01-03 19:29:25 +0400

    adding files and folders from OPE github repository that I modified most of the parts

---
+++ topn_words.py
@@ -0,0 +1,33 @@
+#! /usr/bin/python
+
+# by_AYDIN:
+# usage: python topn_words.py ./data/nyt_voca.txt ./ML-OPE/list_tops.txt topn_output_aydin.txt
+# usage2: python topn_words.py ./data/ap_voca.txt ./ML-OPE/list_tops.txt ap_topn_output_aydin.txt
+
+import sys
+import math
+
+
+def print_topics(vocab_file, nwords, result_file):
+    with open(vocab_file, 'r') as f:
+        vocab = f.readlines()
+
+    vocab = list(map(lambda x: x.strip(), vocab))
+    vocab_index = {i: w for i, w in zip(range(len(vocab)),vocab)}
+
+    with open(nwords, "r") as n:
+        lines = n.readlines()
+        for l in lines:
+            l = l.split()
+            converts = list(map(lambda x: vocab_index[int(x)], l))
+            converts = " ".join(converts)
+            with open (result_file, "a") as r:
+                r.write(converts + "\n")
+
+
+
+if (__name__ == '__main__'):
+    vocab_file = sys.argv[1]
+    nwords = sys.argv[2]
+    result_file = sys.argv[3]
+    print_topics(vocab_file, nwords, result_file)
