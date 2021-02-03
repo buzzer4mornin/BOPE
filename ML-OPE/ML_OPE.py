5decbc553a9ec8ac725655dcfedaadd892afa6f2
ahmadliaydin <aydin.ahmadli@merck.com>
2021-02-03 18:55:42 +0400

    added BOPE algorithm [first draft]

--- ML-OPE/ML_OPE.py
+++ ML-OPE/ML_OPE.py
@@ -3,13 +3,14 @@
 import time
 import numpy as np
 
+
 class MLOPE:
     """
     AYDINS_NOTE: rows --> instances
 
     Implements ML-OPE for LDA as described in "Inference in topic models II: provably guaranteed algorithms".
     """
-    
+
     def __init__(self, num_terms, num_topics, alpha, tau0, kappa, iter_infer):
         """
         Arguments:
@@ -32,12 +33,12 @@ class MLOPE:
         self.kappa = kappa
         self.updatect = 1
         self.INF_MAX_ITER = iter_infer
-        
+
         # Initialize beta (topics)
         self.beta = np.random.rand(self.num_topics, self.num_terms) + 1e-10
-        beta_norm = self.beta.sum(axis = 1)
+        beta_norm = self.beta.sum(axis=1)
         self.beta /= beta_norm[:, np.newaxis]
-        
+
     def static_online(self, batch_size, wordids, wordcts):
         """
         First does an E step on the mini-batch given in wordids and
@@ -62,8 +63,8 @@ class MLOPE:
         start2 = time.time()
         self.m_step(batch_size, wordids, wordcts, theta)
         end2 = time.time()
-        return(end1 - start1, end2 - start2, theta)
-    
+        return end1 - start1, end2 - start2, theta
+
     def e_step(self, batch_size, wordids, wordcts):
         """
         Does e step
@@ -74,9 +75,9 @@ class MLOPE:
         # Inference
         for d in range(batch_size):
             thetad = self.infer_doc(wordids[d], wordcts[d], d)
-            theta[d,:] = thetad
-        return(theta)
-        
+            theta[d, :] = thetad
+        return theta
+
     def infer_doc(self, ids, cts, d):
         """
         Does inference for a document using Online MAP Estimation algorithm.
@@ -88,15 +89,8 @@ class MLOPE:
         Returns inferred theta.
         """
 
-        ###print(self.beta.shape)
         # locate cache memory
-        #try:
         beta = self.beta[:, ids]
-        #except:
-        #    print(ids)
-        #    exit()
-        ###print(beta.shape)
-        ###exit()
 
         # Initialize theta randomly
         theta = np.random.rand(self.num_topics) + 1.
@@ -105,37 +99,89 @@ class MLOPE:
         # x = sum_(k=2)^K theta_k * beta_{kj}
         x = np.dot(theta, beta)
 
-        # Loop
+        p = 0.5
+        T_lower = [1, 0]
+        T_upper = [0, 1]
+
+        for l in range(1, self.INF_MAX_ITER):
+            # ======== Lower ==========
+            if np.random.rand() < p:
+                T_lower[0] += 1
+            else:
+                T_lower[1] += 1
+
+            G_1 = np.dot(beta, cts / x) / p  # maybe outside loop???
+            G_2 = (self.alpha - 1) / theta / (1 - p)  # maybe outside loop???
+
+            ft_lower = T_lower[0] * G_1 + T_lower[1] * G_2
+            index_lower = np.argmax(ft_lower)
+            alpha = 1.0 / (l + 1)
+            theta_lower = np.copy(theta)
+            theta_lower *= 1 - alpha
+            theta_lower[index_lower] += alpha
+
+            # ======== Upper ==========
+            if np.random.rand() < p:
+                T_upper[0] += 1
+            else:
+                T_upper[1] += 1
+
+            ft_upper = T_upper[0] * G_1 + T_upper[1] * G_2
+            index_upper = np.argmax(ft_upper)
+            alpha = 1.0 / (l + 1)
+            theta_upper = np.copy(theta)
+            theta_upper *= 1 - alpha
+            theta_upper[index_upper] += alpha
+
+            # print(theta_upper - theta_lower)
+            # ======== Decision ========
+            x_l = np.dot(cts, np.log(np.dot(theta_lower, beta))) + (self.alpha - 1) * np.log(theta_lower)
+            x_u = np.dot(cts, np.log(np.dot(theta_upper, beta))) + (self.alpha - 1) * np.log(theta_upper)
+
+            compare = np.array([x_l[0], x_u[0]])
+            best = np.argmax(compare)
+
+            if best == 0:
+                theta = theta_lower
+                x = x + alpha * (beta[ , :] - x)
+            else:
+                theta = theta_upper
+                x = x + alpha * (beta[index_upper, :] - x)
+
+        return theta
+
+        '''# Loop
         T = [1, 0]
-        for l in range(1,self.INF_MAX_ITER):
+        for l in range(1, self.INF_MAX_ITER):
             # Pick fi uniformly
             T[np.random.randint(2)] += 1
+
             # Select a vertex with the largest value of  
             # derivative of the function F
             df = T[0] * np.dot(beta, cts / x) + T[1] * (self.alpha - 1) / theta
             index = np.argmax(df)
-            alpha = 1.0 / (l + 1) # why +1 ?? because, otherwise, alpha = 1/1 = 1 ==> theta *= 1 - alpha *= 0 ==> theta = 0 which is baddd
+            alpha = 1.0 / (l + 1)  # why +1 ?? because, otherwise, alpha = 1/1 = 1 ==> theta *= 1 - alpha *= 0 ==> theta = 0 which is baddd
             # Update theta
             theta *= 1 - alpha    # take from all of indices
-            theta[index] += alpha # add to argmax index
+            theta[index] += alpha  # add to argmax index
             # Update x
-            x = x + alpha * (beta[index ,:] - x) # EXPLORE more
-        return(theta)      
-    
+            x = x + alpha * (beta[index, :] - x)  # EXPLORE more
+        return theta'''
+
     def m_step(self, batch_size, wordids, wordcts, theta):
         """
         Does m step: update global variables beta.
         """
         # Compute intermediate beta which is denoted as "unit beta"
-        beta = np.zeros((self.num_topics, self.num_terms), dtype = float)
+        beta = np.zeros((self.num_topics, self.num_terms), dtype=float)
         for d in range(batch_size):
             beta[:, wordids[d]] += np.outer(theta[d], wordcts[d])
         # Check zeros index
-        beta_sum = beta.sum(axis = 0)
+        beta_sum = beta.sum(axis=0)
         ids = np.where(beta_sum != 0)[0]
         unit_beta = beta[:, ids]
         # Normalize the intermediate beta
-        unit_beta_norm = unit_beta.sum(axis = 1)
+        unit_beta_norm = unit_beta.sum(axis=1)
         unit_beta /= unit_beta_norm[:, np.newaxis]
         # Update beta    
         rhot = pow(self.tau0 + self.updatect, -self.kappa)
--- common/utilities.py
+++ common/utilities.py
@@ -31,8 +31,8 @@ def read_data(filename):
 
 def read_data_for_perpl(test_data_folder):
     """ Read data for computing perplexities. """
-    filename_part1 = '%s/data_test_1_part_1.txt'%(test_data_folder)
-    filename_part2 = '%s/data_test_1_part_2.txt'%(test_data_folder)
+    filename_part1 = '%s/data_test_1_part_1.txt' % (test_data_folder)
+    filename_part2 = '%s/data_test_1_part_2.txt' % (test_data_folder)
     (wordids_1, wordcts_1) = read_data(filename_part1)
     (wordids_2, wordcts_2) = read_data(filename_part2)
     return wordids_1, wordcts_1, wordids_2, wordcts_2
@@ -49,15 +49,15 @@ def read_minibatch_list_frequencies(fp, batch_size):
             break
         terms = str.split(line)
         doc_length = int(terms[0])
-        ids = np.zeros(doc_length, dtype = np.int32)
-        cts = np.zeros(doc_length, dtype = np.int32)
-        for j in range(1,doc_length + 1):
+        ids = np.zeros(doc_length, dtype=np.int32)
+        cts = np.zeros(doc_length, dtype=np.int32)
+        for j in range(1, doc_length + 1):
             term_count = terms[j].split(':')
             ids[j - 1] = int(term_count[0])
             cts[j - 1] = int(term_count[1])
         wordids.append(ids)
         wordcts.append(cts)
-    return(wordids, wordcts)
+    return (wordids, wordcts)
 
 
 def read_setting(file_name):
@@ -69,7 +69,7 @@ def read_setting(file_name):
     sets = list()
     vals = list()
     for i in range(len(settings)):
-        #print'%s\n'%(settings[i])
+        # print'%s\n'%(settings[i])
         if settings[i][0] == '#':
             continue
         set_val = settings[i].split(':')
@@ -82,7 +82,7 @@ def read_setting(file_name):
     ddict['batch_size'] = int(ddict['batch_size'])
     ddict['iter_infer'] = int(ddict['iter_infer'])
     ddict['iter_train'] = int(ddict['iter_train'])
-    return(ddict)
+    return (ddict)
 
 
 def compute_perplexities_vb(beta, alpha, eta, max_iter,
@@ -90,16 +90,16 @@ def compute_perplexities_vb(beta, alpha, eta, max_iter,
     """Compute perplexities, employing Variational Bayes."""
     vb = per_vb.VB(beta, alpha, eta, max_iter)
     LD2 = vb.compute_perplexity(wordids_1, wordcts_1, wordids_2, wordcts_2)
-    return(LD2)
+    return (LD2)
 
 
 def compute_sparsity(doc_tp, batch_size, num_topics, _type):
     """Compute document sparsity.
     """
-    sparsity = np.zeros(batch_size, dtype = np.float)
+    sparsity = np.zeros(batch_size, dtype=np.float)
     if _type == 'z':
         for d in range(batch_size):
-            N_z = np.zeros(num_topics, dtype = np.int)
+            N_z = np.zeros(num_topics, dtype=np.int)
             N = len(doc_tp[d])
             for i in range(N):
                 N_z[doc_tp[d][i]] += 1.
@@ -107,10 +107,10 @@ def compute_sparsity(doc_tp, batch_size, num_topics, _type):
     else:
         for d in range(batch_size):
             sparsity[d] = len(np.where(doc_tp[d] < 1e-20)[0])
-            #sparsity[d] = len(np.where(doc_tp[d] > 0.01)[0])  # for Testing Sparsity of 1st theta
+            # sparsity[d] = len(np.where(doc_tp[d] > 0.01)[0])  # for Testing Sparsity of 1st theta
     sparsity /= num_topics
-    return(np.mean(sparsity))
-    #return (sparsity[0])  # for Testing Sparsity of 1st theta
+    return (np.mean(sparsity))
+    # return (sparsity[0])  # for Testing Sparsity of 1st theta
 
 
 def list_top(beta, tops):
@@ -121,13 +121,13 @@ def list_top(beta, tops):
     list_tops = list()
     for k in range(num_tops):
         top = list()
-        arr = np.array(beta[k,:], copy = True)
+        arr = np.array(beta[k, :], copy=True)
         for t in range(tops):
             index = arr.argmax()
             top.append(index)
             arr[index] = min_float
         list_tops.append(top)
-    return(list_tops)
+    return (list_tops)
 
 
 def write_topics(beta, file_name):
@@ -136,9 +136,10 @@ def write_topics(beta, file_name):
     f = open(file_name, 'w')
     for k in range(num_topics):
         for i in range(num_terms - 1):
-            f.write('%.10f '%(beta[k][i]))
-        f.write('%.10f\n'%(beta[k][num_terms - 1]))
+            f.write('%.10f ' % (beta[k][i]))
+        f.write('%.10f\n' % (beta[k][num_terms - 1]))
     f.close()
+
 
 def write_topic_mixtures(theta, file_name):
     batch_size = theta.shape[0]
@@ -146,14 +147,16 @@ def write_topic_mixtures(theta, file_name):
     f = open(file_name, 'a')
     for d in range(batch_size):
         for k in range(num_topics - 1):
-            f.write('%.5f '%(theta[d][k]))
-        f.write('%.5f\n'%(theta[d][num_topics - 1]))
+            f.write('%.5f ' % (theta[d][k]))
+        f.write('%.5f\n' % (theta[d][num_topics - 1]))
     f.close()
+
 
 def write_perplexities(LD2, file_name):
     f = open(file_name, 'a')
-    f.writelines('%f,'%(LD2))
+    f.writelines('%f,' % (LD2))
     f.close()
+
 
 def write_topic_top(list_tops, file_name):
     num_topics = len(list_tops)
@@ -161,33 +164,38 @@ def write_topic_top(list_tops, file_name):
     f = open(file_name, 'w')
     for k in range(num_topics):
         for j in range(tops - 1):
-            f.write('%d '%(list_tops[k][j]))
-        f.write('%d\n'%(list_tops[k][tops - 1]))
+            f.write('%d ' % (list_tops[k][j]))
+        f.write('%d\n' % (list_tops[k][tops - 1]))
     f.close()
+
 
 def write_sparsity(sparsity, file_name):
     f = open(file_name, 'a')
     f.write('%.10f,' % (sparsity))
     f.close()
 
+
 def write_time(i, j, time_e, time_m, file_name):
     f = open(file_name, 'a')
-    f.write('tloop_%d_iloop_%d, %f, %f, %f,\n'%(i, j, time_e, time_m, time_e + time_m))
+    f.write('tloop_%d_iloop_%d, %f, %f, %f,\n' % (i, j, time_e, time_m, time_e + time_m))
     f.close()
+
 
 def write_loop(i, j, file_name):
     f = open(file_name, 'w')
-    f.write('%d, %d'%(i,j))
+    f.write('%d, %d' % (i, j))
     f.close()
 
-#changed ddict.keys() ---> list(ddict.keys())  &&   ddict.values() ---> list(ddict.values())
+
+# changed ddict.keys() ---> list(ddict.keys())  &&   ddict.values() ---> list(ddict.values())
 def write_setting(ddict, file_name):
     keys = list(ddict.keys())
     vals = list(ddict.values())
     f = open(file_name, 'w')
     for i in range(len(keys)):
-        f.write('%s: %f\n'%(keys[i], vals[i]))
+        f.write('%s: %f\n' % (keys[i], vals[i]))
     f.close()
+
 
 def diff_list_tops(list_tops, prev_list_tops, i):
     if i == 1:
@@ -205,29 +213,28 @@ def diff_list_tops(list_tops, prev_list_tops, i):
         print(diff_count)
 
 
-
 def write_file(i, j, beta, time_e, time_m, theta, sparsity, LD2, list_tops, model_folder):
-    #beta_file_name = '%s/beta_%d_%d.dat'%(model_folder, i, j)
-    #theta_file_name = '%s/theta_%d.dat'%(model_folder, i)
-    #per_file_name = '%s/perplexities_%d.csv'%(model_folder, i)
-    #top_file_name = '%s/top%d_%d_%d.dat'%(model_folder, tops, i, j)
-    list_tops_file_name = '%s/list_tops.txt'%(model_folder)
-    #spar_file_name = '%s/sparsity_%d.csv'%(model_folder, i)
-    #time_file_name = '%s/time_%d.csv'%(model_folder, i)
-    #loop_file_name = '%s/loops.csv'%(model_folder)
+    # beta_file_name = '%s/beta_%d_%d.dat'%(model_folder, i, j)
+    # theta_file_name = '%s/theta_%d.dat'%(model_folder, i)
+    # per_file_name = '%s/perplexities_%d.csv'%(model_folder, i)
+    # top_file_name = '%s/top%d_%d_%d.dat'%(model_folder, tops, i, j)
+    list_tops_file_name = '%s/list_tops.txt' % (model_folder)
+    # spar_file_name = '%s/sparsity_%d.csv'%(model_folder, i)
+    # time_file_name = '%s/time_%d.csv'%(model_folder, i)
+    # loop_file_name = '%s/loops.csv'%(model_folder)
 
     # write beta                                    ##
-    #if j % 10 == 1:                                 ##
+    # if j % 10 == 1:                                 ##
     #    write_topics(beta, beta_file_name)          ##
     # write theta                                   ##
-    #write_topic_mixtures(theta, theta_file_name)    ##
+    # write_topic_mixtures(theta, theta_file_name)    ##
     # write perplexities
-    #write_perplexities(LD2, per_file_name)
+    # write_perplexities(LD2, per_file_name)
     # write list top
     write_topic_top(list_tops, list_tops_file_name)
     # write sparsity
-    #write_sparsity(sparsity, spar_file_name)
+    # write_sparsity(sparsity, spar_file_name)
     # write time
-    #write_time(i, j, time_e, time_m, time_file_name)
+    # write_time(i, j, time_e, time_m, time_file_name)
     # write loop
-    #write_loop(i, j, loop_file_name)
+    # write_loop(i, j, loop_file_name)
